d1fca0b4fc56b7ce6b296ef2077db106e2f73e1c
Ankit Jaiswal <jankit360@gmail.com>
2020-12-15 01:46:56 +0530

    Update settings.py

--- DjangoProject/GateEasy/settings.py
+++ DjangoProject/GateEasy/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'practise_mock.apps.PractiseMockConfig',
     'Gate.apps.GateConfig',
     'MECH.apps.MechConfig',
     'django.contrib.admin',
