b450c3df96d955ec949f6aec4c27ac914b3aa8db
Ankit Jaiswal <jankit360@gmail.com>
2020-12-18 20:02:46 +0530

    Update models.py

--- DjangoProject/practise_mock/models.py
+++ DjangoProject/practise_mock/models.py
@@ -7,6 +7,7 @@ class MockTest(models.Model):
     subject = models.CharField(max_length=500)
     mock_no = models.IntegerField()
     mock = models.JSONField(default=dict)
+    opt_answer = models.JSONField(default=[{}])
 
     def __str__(self):
          return self.Department+ " " + self.subject + "( "+str(self.mock_no)+" )"
