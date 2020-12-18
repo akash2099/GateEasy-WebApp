89139c38b82d1e92efd377fa89188b995774c5c3
Ankit Jaiswal <jankit360@gmail.com>
2020-12-18 20:04:08 +0530

    Update views.py

--- DjangoProject/practise_mock/views.py
+++ DjangoProject/practise_mock/views.py
@@ -1,19 +1,11 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse, get_object_or_404
 from .models import MockTest
 from django.http import Http404
 from django.utils.safestring import SafeString
-
+import json
 
 # Create your views here.
-'''
-def index(request):
-    all_albums = Album.objects.all()
-    html =''
-    for album in  all_albums:
-        url  = '/music/' + str(album.id) +'/'
-        html +='<a href="'+ url +'">'+ album.album_title +'</a><br>'
-    return HttpResponse(html)
-'''
+
 def index(request):
     all_albums = MockTest.objects.all()
     context = {
@@ -27,3 +19,19 @@ def details(request, album_id):
     except MockTest.DoesNotExist:
         raise Http404("MockTest Does not exist")
     return render(request,'mock.html',{'mock' : SafeString(mockt.mock),'info': mockt})
+
+def result(request, album_id):
+    album = get_object_or_404(MockTest, pk=album_id)
+    if request.method =='POST':
+        post_data=json.loads(request.body)
+        album.opt_answer= post_data
+        album.save()
+        
+    try:
+        mockt = MockTest.objects.get(pk=album_id)
+    except MockTest.DoesNotExist:
+        raise Http404("MockTest Does not exist")
+    return render(request,'mock_result.html',{'mock' : SafeString(mockt.mock),'info': SafeString(mockt.opt_answer['save']), 'obj' : mockt})
+
+
+   
